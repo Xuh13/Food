87b1049723220ae1e82254e6a0ab43c2d2b2728e
MadHat13 <1124298082@qq.com>
2020-03-26 11:21:04 +0800

    3.26

--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -1,18 +1,31 @@
 // pages/category/category.js
 Page({
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+      }, 200)
+    })
+  },
+  selectResult: function (e) {
+    console.log('select result', e.detail)
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inputShowed: false,
+    inputVal: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      search: this.search.bind(this)
+    })
   },
 
   /**
